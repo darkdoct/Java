a173e2fc5f8025d9749f29b72cef2b2c0de1d54b
darkdoct <neelpatelninjahousecat@gmail.com>
2019-06-21 10:39:52 -0400

    Understandin the main method

---
+++ src/com/neel/java/Welcome.java
@@ -0,0 +1,18 @@
+package com.neel.java;
+
+public class Welcome {
+
+	//staic meaning you dont have to create an instance or object of the class
+	//[] array of arguments because the size is anything 
+	//main is needed for the interpreter to interpret, but it will compile just wont interpret
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Neel's Java Tutorial");
+		// TODO Auto-generated method stub
+
+	}
+
+}   
+
+
